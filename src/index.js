303228667577c5c8bdaaaa46d00555d7c8af2ba6
Tommy Berglind <tommy@tessin.se>
2018-10-09 22:50:28 +0200

    WIP

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+console.log("running index.js");
+ReactDOM.render(<App />, document.getElementFromId("app"));
